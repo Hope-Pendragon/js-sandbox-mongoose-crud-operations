510334689fb31c426b7647af487aeec0116e5c5d
Hope-Pendragon <108218691+Hope-Pendragon@users.noreply.github.com>
2022-12-27 21:29:21 +0200

    create database connection handler

---
+++ database/connection.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export default function connectToDatabase() {
+
+	mongoose.connect( process.env.MONGODB_URI, {
+		dbName: process.env.MONGODB_DB,	
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	} ).then( console.log( `Database connection established.` ) );
+
+}
