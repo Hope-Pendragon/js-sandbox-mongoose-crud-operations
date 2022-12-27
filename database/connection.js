33f7d45549ff3435ae63b48f75452d1e485f7f57
Hope-Pendragon <108218691+Hope-Pendragon@users.noreply.github.com>
2022-12-28 00:43:21 +0200

    Fix add missing async

--- database/connection.js
+++ database/connection.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
 
-export default function connectToDatabase() {
-
-	mongoose.connect( process.env.MONGODB_URI, {
-		dbName: process.env.MONGODB_DB,	
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	} )
-	.then( console.log( `Database connection established.` ) )
-	.catch( ( error ) => {
-		return Promise.reject(error);
-	} );
-
+export default async function connectToDatabase() {
+	mongoose
+		.connect(process.env.MONGODB_URI, {
+			dbName: process.env.MONGODB_DB,
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		})
+		.then(console.log(`Database connection established.`))
+		.catch((error) => {
+			return Promise.reject(error);
+		});
 }
