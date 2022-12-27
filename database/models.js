2e5ee530faa9dbd397406b6e4cdb3d9e28d9c0a6
Hope-Pendragon <108218691+Hope-Pendragon@users.noreply.github.com>
2022-12-27 21:32:53 +0200

    create schema and model for TestEntry document

---
+++ database/models.js
@@ -0,0 +1,9 @@
+import {  Schema, model, models } from "mongoose";
+
+const testEntrySchema = new Schema({
+	text: String
+}, {
+	timestamps: true
+});
+
+export default models.TestEntry || model("TestEntry", testEntrySchema);
