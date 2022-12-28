c4b50d0f098561eef0fd2a2fa5ca1900a56e81b5
Hope-Pendragon <a.void.ant.hopeful@gmail.com>
2022-12-28 16:52:32 +0200

    Add additional error handling and connection info

--- database/connection.js
+++ database/connection.js
@@ -1,14 +1,28 @@
 import mongoose from "mongoose";
 mongoose.set("strictQuery", false);
 export default async function connectToDatabase() {
-	mongoose
-		.connect(process.env.MONGODB_URI, {
+	mongoose.connection.on("error", (error) => {
+		console.log(`An error has occurred with the database connection.`);
+		return Promise.reject(error);
+	});
+
+	mongoose.connection.on("disconnected", () => {
+		console.log(`Not connected to database.`);
+	});
+
+	try {
+		mongoose.connect(process.env.MONGODB_URI, {
 			dbName: process.env.MONGODB_DB,
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
-		})
-		.then(console.log(`Database connection established.`))
-		.catch((error) => {
-			return Promise.reject(error);
 		});
+		console.log(`Database connection established.`);
+	} catch (error) {
+		console.log(`Unable to established database connection.`);
+		return Promise.reject(error);
+	}
+
+	mongoose.connection.on("connected", () => {
+		console.log(`Connected to database.`);
+	});
 }
