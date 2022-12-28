517e764a838789c6c3614e20bc3b9f17c31fe87f
Hope-Pendragon <108218691+Hope-Pendragon@users.noreply.github.com>
2022-12-28 02:56:56 +0200

    Fix put controller to work as intended
    The section is heavily commented to justify each individual part
    The put controller is quite sensitive to changes

--- database/controllers.js
+++ database/controllers.js
@@ -1,22 +1,25 @@
+import { ObjectId } from "mongodb";
 import TestEntry from "./models";
 
-export async function getTestEntries( _req, res ) {
+export async function getTestEntries(_req, res) {
 	try {
 		const testEntries = await TestEntry.find();
 
 		res.status(200).json(testEntries);
-	} catch ( error ) {
-		res.status(404).send( { error: error } );
+	} catch (error) {
+		res.status(404).send({ error: error });
 	}
 }
 
-export async function getTestEntry( req, res) {
+export async function getTestEntry(req, res) {
 	try {
 		const _id = req.query;
 
-		const testEntry = await TestEntry.findByID( _id );
+		const testEntry = await TestEntry.findByID(_id);
 
-		if ( !testEntry ) { res.status( 404 ).send( { error: "No data received." } ) }
+		if (!testEntry) {
+			res.status(404).send({ error: "No data received." });
+		}
 
 		res.status(200).json(testEntry);
 	} catch (error) {
@@ -24,7 +27,7 @@ export async function getTestEntry( req, res) {
 	}
 }
 
-export async function postTestEntry (req, res) {
+export async function postTestEntry(req, res) {
 	try {
 		const testEntry = JSON.parse(req.body);
 
@@ -32,31 +35,49 @@ export async function postTestEntry (req, res) {
 			res.status(404).send({ error: "Unable to create entry..." });
 		}
 
-		TestEntry.create(testEntry, ( error, data ) => {
-			if ( error ) {
-				res.status(404).send({ error: error } );
+		TestEntry.create(testEntry, (error, data) => {
+			if (error) {
+				res.status(404).send({ error: error });
 			}
 
-			res.status( 200 ).json( data );
+			res.status(200).json(data);
 		});
 	} catch (error) {
 		res.status(404).send({ error: error });
 	}
 }
 
-export async function putTestEntry (req, res) {
+export async function putTestEntry(req, res) {
 	try {
+		// The following code needs to be executed EXACTLY as is
+		// - Changing any one part will likely require almost everything else to change as well
+
+		// - _id MUST come from the query and both the property or value should be unquoted string values
 		const { _id } = req.query;
-		const updates = JSON.parse(req.body);
-		
-		const testEntry = TestEntry.findByIdAndUpdate(_id, updates, { new: true });
-		
 		if (!_id) {
 			res.status(404).send({ error: "No entry selected to update..." });
 		}
+
+		// - updates MUST be set directly equal to req.body
+		// - the contents of the body MUST be sent as JSON
+		//   - and due to that property names MUST be DOUBLE quoted
+		// - neither updates nor req.body should be modified in any way
+		const updates = req.body;
 		if (!updates) {
 			res.status(404).send({ error: "No updates provided..." });
 		}
+
+		// - testEntry needs to be defined and be const
+		// - await MUST be used
+		// - the query function MUST be assigned directly to testEntry
+		// - neither the _id nor updates should be passed in any way other than as is
+		// - the properties being passed must already exist within the database to be updated
+		//   - { strict: false } MUST be specified in the options if you wish to bypass this
+		const testEntry = await TestEntry.findByIdAndUpdate(_id, updates, {
+			strict: false,
+			strictQuery: false,
+			new: true,
+		});
 
 		res.status(200).json(testEntry);
 	} catch (error) {
@@ -64,8 +85,7 @@ export async function putTestEntry (req, res) {
 	}
 }
 
-
-export async function deleteTestEntry (req, res) {
+export async function deleteTestEntry(req, res) {
 	try {
 		const { _id } = req.query;
 		const testEntry = await TestEntry.findByIdAndDelete(_id);
@@ -76,6 +96,6 @@ export async function deleteTestEntry (req, res) {
 
 		res.status(200).json({ deleted: testEntry });
 	} catch (error) {
-		res.status(404).send( {  error: error } );
+		res.status(404).send({ error: error });
 	}
 }
